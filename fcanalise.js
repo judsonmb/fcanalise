a355cbcdd8c0368381ae05d69bdb9aaf822c41aa
Judson <judsonmelobandeira@gmail.com>
2022-05-17 12:15:49 -0300

    separando as rotas em handlers e criando testes unitários para as páginas criadas.

--- fcanalise.js
+++ fcanalise.js
@@ -1,6 +1,7 @@
+//to get routes,
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
 
 const app = express()
 
@@ -16,23 +17,13 @@ const port = process.env.PORT || 3000
 //routes
 app.use(express.static(__dirname + '/public'))
 
-//searches home in views directory. on the rest of routes happens the same
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; `
